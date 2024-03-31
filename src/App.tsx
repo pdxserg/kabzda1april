2d7500923c9dd1c32bb813ea52e6472d2880c3db
SergeyStarchenko <pdxserg@gmail.com>
2024-03-30 18:53:14 -0700

    file Accordion.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from "./components/accordion/Accordion";
 
 function App() {
-    return (
-        <div className="App">
-           hell
-        </div>
-    );
+	return (
+		<div className="App">
+			<Rating/>
+			<Accordion />
+			<Rating/>
+
+		</div>
+	)
 }
 
+
 export default App;
+
+function Rating() {
+	console.log("How many time rendered REITING")
+	return (
+		<>
+		<Star />
+		<Star />
+		<Star />
+		<Star />
+		<Star />
+		</>
+
+	)
+}
+function Star () {
+	console.log("How many time rendered STAR")
+	return(
+		<div>
+			<div>star</div>
+		</div>
+	)
+}
+
---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export function Accordion() {
+	console.log("Accordion")
+	return (
+		<>
+			<AccordionTitle/>
+			<AccordionBody/>
+		</>
+	)
+}
+
+function AccordionTitle() {
+	console.log("How many time rendered AccordionTitle")
+	return (
+		<>
+			<h3>Menu</h3>
+		</>
+	)
+
+}
+
+function AccordionBody() {
+	console.log("How many time rendered AccordionBody")
+	return (
+		<>
+			<ul>
+				<li>1</li>
+				<li>2</li>
+				<li>3</li>
+			</ul>
+		</>
+	)
+}

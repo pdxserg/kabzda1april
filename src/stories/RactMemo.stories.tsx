f24a233b131fb4ce7189a84f9dda38a517956044
SergeyStarchenko <pdxserg@gmail.com>
2024-06-09 16:29:55 -0700

    callBackMemo.stories create

--- src/stories/RactMemo.stories.tsx
+++ src/stories/RactMemo.stories.tsx
@@ -1,9 +1,7 @@
 import React, {useState} from "react";
-
 export default {
 	title: 'React.memo demo'
 }
-
 
 const SetCounter = (props: { count: number }) => {
 	console.log("count")
@@ -11,14 +9,7 @@ const SetCounter = (props: { count: number }) => {
 }
 const Counter= React.memo(SetCounter)
 
-
-
-
-type UsersType = {
-	users: Array<string>
-}
-
-const SupUsers = (props: UsersType) => {
+const SupUsers = (props: { users: Array<string> }) => {
 	console.log("name")
 	return <div>
 		{props.users.map((u, i) => <div key={i}>{u}</div>)}
@@ -27,22 +18,50 @@ const SupUsers = (props: UsersType) => {
 const Users= React.memo(SupUsers)
 
 
-
-
-
 export const Example1 = () => {
-	const [count, setCount] = useState(2)
+	const [count, setCount] = useState(0)
 	const [users, setUsers] = useState(['Bob', 'Chack', 'Den'])
 	const handler = ()=> {
 		setUsers([...users, 'ddd'])
 	}
 
-
 	return <>
-		<button onClick={handler}>new user</button>
-		<button onClick={() => setCount(count + 1)}>+</button>
 		<Counter count={count}/>
 		<Users users={users}/>
+		<button onClick={handler}>new user</button>
+		<button onClick={() => setCount(count + 1)}>+</button>
+	</>
+}
 
+
+const SetCounter2 = (props: { count: number }) => {
+	console.log("count")
+	return <div>{props.count}</div>
+}
+const Counter2= React.memo(SetCounter2)
+
+const SupBooks = (props: { books: Array<string> }) => {
+	console.log("book")
+	return <div>
+		{props.books.map((b, i) => <div key={i}>{b}</div>)}
+	</div>
+}
+
+const Books= React.memo(SupBooks)
+
+
+export const Example2 = () => {
+	const [count, setCount] = useState(0)
+	const [books, setUsers] = useState(['JS', 'HTML', 'CSS'])
+	console.log(books)
+	const handler = ()=> {
+		setUsers([...books, 'React'])
+	}
+
+	return <>
+		<Counter2 count={count}/>
+		<Books books={books}/>
+		<button onClick={handler}>new book</button>
+		<button onClick={() => setCount(count + 1)}>+</button>
 	</>
 }
--- src/stories/assets/UseMemo.stories.tsx
+++ src/stories/assets/UseMemo.stories.tsx
@@ -3,10 +3,49 @@ import {array} from "prop-types";
 
 export default {title: 'useMemo'}
 
-const SupUsers1 = (props: { users: Array<string> }) => {
-	console.log("name")
+
+
+const SuperUser2 = (props : {b:Array<string> })=>{
+	console.log("superuser")
+
+	return(
+		<div>
+			<input list="ddd"/>
+			<datalist id="ddd">
+				{props.b.map(b => <option value={b}></option>)}
+			</datalist>
+			<button onClick={()=>{}}>+</button>
+		</div>
+	)
+}
+const User2= React.memo(SuperUser2)
+export const example2=()=>{
+	console.log("eee")
+		const [a, setA] = useState(0)
+	const [b, setB]=useState<Array<string>>(['ddad','eee','qqAq'])
+
+const newArr=useMemo(()=>{
+		const newArr= b.filter(b=> b.toLowerCase().indexOf("a")>-1)
+	return newArr
+	},[b])
+
+
 	return <div>
-		{props.users.map((u, i) => <div key={i}>{u}</div>)}
+		{a}
+		<button onClick={() => setA(a + 1)}>+</button>
+		<User2 b={newArr}/>
+	</div>
+}
+//------------------------------------
+const SupUsers1 = () => {
+	console.log("numbers")
+	const [a, setA]= useState(0)
+	const handlrer=()=>{
+		setA(a+1)
+	}
+	return <div>
+		<input value={a} />
+		<button onClick={handlrer}>+</button>
 	</div>
 }
 const Users1 = React.memo(SupUsers1)
@@ -28,26 +67,28 @@ export const List = () => {
 					title: "Belarus",
 					city: {
 						name: "Minsk",
-						people: 100}}},
+						people: 200}}},
 		 {id: 2,
 				country: {
 					title: "Kazakhstan",
 					city: {
 						name: "Astana",
-						people: 100}}},
+						people: 400}}},
 		 {id: 3,
 				country: {
 					title: "Uzbek",
 					city: {
 						name: "Bishkek",
-						people: 100}}},
+						people: 1000}}},
 		]
 	)
 	const newArrayMemo = useMemo(() => {
-		const newArray = users.filter(u => u.country.title.toLowerCase().indexOf("a") > -1)
+		console.log("help to react memo")
+		const newArray = users.filter(u => u.country.city.people > 399)
 		return newArray
 	}, [users])
 	return <>
+		<Users1/>
 		<select>
 			{newArrayMemo.map(c => (<option value={c.id}>{c.country.city.name}</option>))}
 		</select>
@@ -68,14 +109,14 @@ export const Example1 = () => {
 	console.log("help to react memo")
 	const [count, setCount] = useState(2)
 	const [users, setUsers] = useState(['Bob', 'Chack', 'Den'])
-	const newArrayMemo = useMemo(() => {
-		const newArray = users.filter(u => u.toLowerCase().indexOf("a") > -1)
-		return newArray
-	}, [users])
+	// const newArrayMemo = useMemo(() => {
+	 	const newArray = users.filter(u => u.toLowerCase().indexOf("a") > -1)
+	// 	return newArray
+	// }, [users])
 	return <>
 		<button onClick={() => setCount(count + 1)}>+</button>
 		{count}
-		<Users users={newArrayMemo}/>
+		<Users users={newArray}/>
 	</>
 }
 // -------------------------------------------------------------------
@@ -86,16 +127,16 @@ export const UseMemoStories = () => {
 	let resultA = 1
 	let resultB = 1
 
-	resultA = useMemo(() => {
+	 resultA = useMemo(() => {
 		let tempResultA = resultA
 		for (let i = 1; i <= a; i++) {
 			console.log("A :" + i)
 			tempResultA = tempResultA * i
 		}
-		return tempResultA
-	}, [a]);
+	 	return tempResultA
+	 }, [a]);
 
-	resultB = useMemo(() => {
+	 resultB = useMemo(() => {
 		for (let i = 1; i <= b; i++) {
 			console.log("B :" + i)
 			// setTimeout(function() {
@@ -103,8 +144,8 @@ export const UseMemoStories = () => {
 			// }, i * 1000);
 			resultB = resultB * i
 		}
-		return resultB
-	}, [b]);
+	 	return resultB
+	 }, [b]);
 
 
 	return (
